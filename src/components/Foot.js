d946dfe63a28398e394421da55889e63559b5e82
Tarmo <n4378@student.jamk.fi>
2021-07-22 00:22:49 +0300

    Footeri added

---
+++ src/components/Foot.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {Link} from 'gatsby';
+
+export default function Foot() {
+  return (
+      <div className='foot'>
+        Y-Tunnus 0123124512 | Tarmo Urrio
+      </div>
+  )
+}
